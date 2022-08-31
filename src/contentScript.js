282d4c078d851a4914498db28964c7b982e03f89
Ediz T <ediz.tefenlilioglu@gmail.com>
2022-08-31 14:47:02 +0200

    add Button and CSS

--- src/contentScript.js
+++ src/contentScript.js
@@ -29,4 +29,40 @@
 
   loadExpertTokens();
 
+
+  function addPreisDetektivToSite() {
+    const element = document.getElementsByClassName('widget-ArticleStatus-statusPoint')[0];
+    if (element) {
+      // Append Div to Site
+      const div = document.createElement('div');
+      div.className = 'widget-ArticleStatus-button-wrapper';
+      element.appendChild(div);
+
+      // Append Button to Site and restyle it
+      const button = document.createElement('button');
+      button.innerHTML = '💸 Expert Bestpreis finden';
+      button.id = 'bestpreis-button';
+      div.appendChild(button);
+      button.onclick = function () {
+        setDisplay('bestpreis-overlay', 'block');
+      }
+
+      // Add Overlay to Site
+      const overlay = document.createElement('div');
+      overlay.id = 'bestpreis-overlay';
+      overlay.onclick = function () {
+        setDisplay('bestpreis-overlay', 'none');
+      }
+      element.appendChild(overlay);
+    }
+
+  }
+
+  addPreisDetektivToSite();
+
+
+  function setDisplay(Id, attr) {
+    document.getElementById(Id).style.display = attr;
+  }
+
 })();
